f0a6fc8e2c35981374121997d6cbd2a81ebcb17f
IsmayilJafarov <120932515+IsmayilJafarov@users.noreply.github.com>
2023-04-24 14:15:20 +0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const cols = document.querySelectorAll('.col');
+
+document.addEventListener('keydown', (e) => {
+    if (e.code.toLowerCase = 'space') {
+        setRandomColors()
+    }
+})
+
+function setRandomColors() {
+    cols.forEach((col) => {
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = chroma.random()
+        
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+
+function setTextColor (text, color) {
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors()
